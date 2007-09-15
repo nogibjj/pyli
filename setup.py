295c2599ef8e1a4ec0a17949ac72fd6b0be4a6b1
noah.gift <devnull@localhost>
2007-09-15 07:30:16 +0000

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# liten 0.1.2 -- deduplication command line tool
+# 
+# Author: Noah Gift
+
+from ez_setup import use_setuptools
+use_setuptools(min_version="0.6a9")
+
+from setuptools import Extension, find_packages, setup
+
+
+setup(name='liten',
+      version=0.1.2,
+      description='a de-duplication command line tool',
+      long_description="This command line tool will examine a file system and report back duplicates using a md5 checksum hash algorithm.",
+      author='Noah Gift',
+      author_email='noah.gift@gmail.com',
+      url='http://zooko.com/repos/pyutil',
+      license='GNU GPL3',
+      packages=find_packages(),
+      classifiers=trove_classifiers
+      zip_safe=False, # I prefer unzipped for easier access.
+      )
+
