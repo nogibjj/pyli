613d409d794054951079d2aa48022cc5e479b37b
noah.gift <devnull@localhost>
2008-04-12 18:03:29 +0000

    adding updates

--- setup.py
+++ setup.py
@@ -27,20 +27,14 @@ setup(
               'Intended Audience :: Developers',
               'License :: OSI Approved :: MIT License',
             ],
-      keyword='liten cli deduplication',
       author='Noah Gift',
       author_email='noah.gift@gmail.com',
       url='http://pypi.python.org/pypi/liten',
+      download_url="http://code.google.com/p/liten/downloads/list",
       license='MIT',
       py_modules=['virtualenv'],
       zip_safe=False,
       py_modules=['liten'],
-      ## Hacks to get the package data installed:
-      #copied from ian bicking.
-      packages=[''],
-      package_dir={'': '.'},
-      #package_data={'': ['support-files/*-py%s.egg' % sys.version[:3]]},
-      zip_safe=False,
       entry_points="""
       [console_scripts]
       liten = liten:main
